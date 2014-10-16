2c939f65ef4944ca29728aec2adba4e8095be228
Howard.Zuo <leftstick@qq.com>
2014-10-16 19:51:43 +0800

    update docs

--- demo/demo.js
+++ demo/demo.js
@@ -1,30 +1,35 @@
 'use strict';
 var demo = angular.module('demo', ['angular-smart-chartist']);
 
-demo.controller('DemoController', function($scope, $timeout) {
-    $scope.tooltipStatus = true;
-    $scope.data = {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+demo.controller('DemoController', function($scope) {
+    $scope.barData = {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         series: [
-        {
-            name: 'Title1',
-            data: [12, 9, 7, 8, 5]
-        },{
-            name: 'Title2',
-            data: [2, 1, 3.5, 7, 3]
-        },{
-            name: 'Title3',
-            data: [1, 3, 4, 5, 6]
+            [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+            [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+        ]
+    };
+
+    $scope.barOpts = {
+        seriesBarDistance: 10
+    };
+
+    $scope.lineTooltip = true;
+    $scope.lineData = {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        series: [{
+            name: 'Fibonacci sequence',
+            data: [1, 2, 3, 5, 8, 13]
+        }, {
+            name: 'Golden section',
+            data: [1, 1.618, 2.618, 4.236, 6.854, 11.09]
         }]
     };
 
-    // $timeout(function() {
-    //     $scope.options = {
-    //         axisY: {
-    //             showLabel: false
-    //         },
-    //         width: 300,
-    //         height: 200
-    //     };
-    // }, 5000);
+
+    $scope.pieData = {
+        labels: ['Bananas', 'Apples', 'Grapes'],
+        series: [20, 15, 40]
+    };
+
 });
