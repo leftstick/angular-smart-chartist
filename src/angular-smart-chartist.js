57e46f0e4bc5959b1800d10b903be4739feacbc3
Howard.Zuo <leftstick@qq.com>
2015-02-27 19:37:06 +0800

    fix issue that tooltip displayed incorrect in FF

--- src/angular-smart-chartist.js
+++ src/angular-smart-chartist.js
@@ -93,9 +93,17 @@
                         });
 
                         element.on('mousemove', function(event) {
+
+                            if(typeof event.offsetX === 'undefined'){
+                                //the workaround here cause FF doesn't have offsetX/Y attribute
+                                event.offsetX = event.originalEvent.layerX;
+                                event.offsetY = event.originalEvent.layerY;
+                            }
+                            var left = event.offsetX - $toolTip.width() / 2 - 8;
+                            var top = event.offsetY - $toolTip.height() - 40;
                             $toolTip.css({
-                                left: event.offsetX - $toolTip.width() / 2 - 8,
-                                top: event.offsetY - $toolTip.height() - 40
+                                left: left,
+                                top: top
                             });
                         });
                     };
